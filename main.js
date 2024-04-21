5fd7e324d464cd4bf3c1ab521fc1169e115bd405
Tumaini <tumainikebaya.234@gmail.com>
2024-04-21 22:22:01 +0300

    Adds main.js

--- main.js
+++ main.js
@@ -3,10 +3,58 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+/*
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const likeButtons = document.querySelectorAll(".like-glyph");
+  
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall() 
+        .then(() => {
+          // Server call successful
+          button.classList.add("activated-heart");
+          button.innerHTML = FULL_HEART;
+        })
+        .catch(() => {
+          // Server call failed
+          errorModal.classList.remove("hidden");
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+*/
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  //errorModal.classList.add("hidden");
+  const likeButtons = document.querySelectorAll(".like-glyph");
 
-
-
-
+  // Add event listener to each like button
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // If server call succeeds, change heart to full heart
+          button.classList.add("activated-heart");
+          button.innerText = "♥";
+        })
+        .catch(() => {
+          // If server call fails, display error modal
+          
+          const errorMessage = document.getElementById("modal-message");
+          errorMessage.innerText = "Server Error. Please try again later.";
+          setTimeout(() => {
+            // Hide error modal after 3 seconds
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+errorModal.classList.add("hidden");
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
